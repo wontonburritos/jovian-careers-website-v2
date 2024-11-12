a8e30ac42611fa2ff4cc55cb7c383656cba04824
wontonburritos <jayfrisch@sbcglobal.net>
2024-11-11 16:08:20 -0800

    commenting out jsonify

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def hello_jovian():
     jobs = load_jobs_from_db()
     return render_template('home.html', jobs=jobs, company_name ='Jovian')
 
-@app.route('/api/jobs')
-def list_jobs():
-    return jsonify(JOBS)
+#@app.route('/api/jobs')
+#def list_jobs():
+#    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', debug=True, port=5001)
