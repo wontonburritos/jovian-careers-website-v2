e89f5ece87ad9202ef38966c5debb169cb08d738
git <jayfrisch@sbcglobal.net>
2024-11-11 18:55:49 -0800

    removed api/jobs

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from flask import Flask, render_template#, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("select title, location, salary from jobs"))
-        jobs = []
-        for row in result.all():
-            jobs.append(row)
-        print(jobs)
-        return jobs
 
 @app.route('/')
 def hello_jovian():
@@ -20,7 +10,8 @@ def hello_jovian():
 
 #@app.route('/api/jobs')
 #def list_jobs():
-#    return jsonify(JOBS)
+#    jobs = load_jobs_from_db()
+#    return jobs
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', debug=True, port=5001)
--- database.py
+++ database.py
@@ -1,3 +1,11 @@
 from sqlalchemy import create_engine, text
 db_connection_string = "postgresql://postgres.zpzcgloiyendcjgqpwsx:o3UhJk81SGmwCnMl@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
 engine = create_engine(db_connection_string)
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(row)
+        return jobs
