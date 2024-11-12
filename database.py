04225e2cf3d3b872557c6563b621d86c9ef1d4bf
wontonburritos <jayfrisch@sbcglobal.net>
2024-11-11 16:10:45 -0800

    shortened engine in db_connection_string

--- database.py
+++ database.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine, text
 db_connection_string = "postgresql://postgres.zpzcgloiyendcjgqpwsx:o3UhJk81SGmwCnMl@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
-engine = create_engine(db_connection_string, echo=True)
+engine = create_engine(db_connection_string)
