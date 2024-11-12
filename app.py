e4c22774d7b32e1648d72992e3461412c62aee28
wontonburritos <jayfrisch@sbcglobal.net>
2024-11-12 07:34:46 -0800

    fixed api/jobs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template#, jsonify
+from flask import Flask, render_template, jsonify
 from database import load_jobs_from_db
 
 app = Flask(__name__)
@@ -8,10 +8,10 @@ def hello_jovian():
     jobs = load_jobs_from_db()
     return render_template('home.html', jobs=jobs, company_name ='Jovian')
 
-#@app.route('/api/jobs')
-#def list_jobs():
-#    jobs = load_jobs_from_db()
-#    return jobs
+@app.route('/api/jobs')
+def list_jobs():
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', debug=True, port=5001)
