e62262a4ceccd2160fd8e7664e1af867a3a1d31c
git <jayfrisch@sbcglobal.net>
2024-11-12 20:17:03 -0800

    got rid of dict again

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ def hello_jovian():
     jobs = load_jobs_from_db()
     return render_template('home.html', jobs=jobs, company_name ='Jovian')
 
-@app.route('/api/jobs')
-def list_jobs():
-    jobs = load_jobs_from_db()
-    return jsonify(jobs)
+# @app.route('/api/jobs')
+# def list_jobs():
+#     jobs = load_jobs_from_db()
+#     return jsonify(jobs)
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', debug=True, port=5001)
